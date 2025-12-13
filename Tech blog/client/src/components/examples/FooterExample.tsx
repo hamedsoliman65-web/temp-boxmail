286df29b6fabd313cfffe9a3ca26a43a9b6677ee
hamedsoliman65-web <hamed.soliman65@gmail.com>
2025-12-13 08:03:53 +0200

    Delete Tech blog directory

--- Tech blog/client/src/components/examples/FooterExample.tsx
+++
@@ -1,5 +0,0 @@
-import Footer from "../blog/Footer";
-
-export default function FooterExample() {
-  return <Footer />;
-}
--- Tech blog/client/src/components/examples/HeaderExample.tsx
+++
@@ -1,5 +0,0 @@
-import Header from "../blog/Header";
-
-export default function HeaderExample() {
-  return <Header onSearch={(q) => console.log("Search:", q)} />;
-}
