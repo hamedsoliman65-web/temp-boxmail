da1cce33571eb64b6434645251d513d287824b84
hamedsoliman65-web <hamed.soliman65@gmail.com>
2026-05-28 05:39:54 +0300

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,70 +1,53 @@
-<script async
-src="https://www.googletagmanager.com/gtag/js?id=AW-10997329046">
-</script>
-
-<script>
-
+// 1. تهيئة الـ dataLayer والدوال الأساسية فوراً لمنع أي تأخير
 window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
 
-function gtag() {
-  dataLayer.push(arguments);
-}
-
-gtag('js', new Date());
-
-/* الوضع الافتراضي قبل موافقة المستخدم */
+// 2. إرسال أمر منع التتبع الافتراضي (Consent) قبل تحميل أي سكريبت خارجي من جوجل
 gtag('consent', 'default', {
-  ad_storage: 'denied',
-  analytics_storage: 'denied',
-  ad_user_data: 'denied',
-  ad_personalization: 'denied',
-  wait_for_update: 500
+  'ad_storage': 'denied',
+  'analytics_storage': 'denied',
+  'ad_user_data': 'denied',
+  'ad_personalization': 'denied',
+  'wait_for_update': 500
 });
 
-/* Google Ads */
-gtag('config', 'AW-10997329046');
-
-/* Google Analytics */
-gtag('config', 'G-NR4CFG9TFJ', {
-  anonymize_ip: true
-});
-
-/* منع التحميل المتكرر */
+// 3. تعريف دالة الموافقة وجعلها متاحة فوراً في الـ window للأزرار في أسفل الموقع
 window.analyticsLoaded = false;
-
-/* تغيير حالة الموافقة */
 window.setConsent = function(status) {
-
   if (status === 'granted') {
-
     gtag('consent', 'update', {
-      ad_storage: 'granted',
-      analytics_storage: 'granted',
-      ad_user_data: 'granted',
-      ad_personalization: 'granted'
+      'ad_storage': 'granted',
+      'analytics_storage': 'granted',
+      'ad_user_data': 'granted',
+      'ad_personalization': 'granted'
     });
 
     if (!window.analyticsLoaded) {
-
       window.analyticsLoaded = true;
-
+      // إرسال حدث مشاهدة الصفحة يدوياً بعد تفعيل الموافقة
       gtag('event', 'page_view', {
         page_path: location.pathname
       });
-
     }
-
   } else {
-
     gtag('consent', 'update', {
-      ad_storage: 'denied',
-      analytics_storage: 'denied',
-      ad_user_data: 'denied',
-      ad_personalization: 'denied'
+      'ad_storage': 'denied',
+      'analytics_storage': 'denied',
+      'ad_user_data': 'denied',
+      'ad_personalization': 'denied'
     });
-
   }
-
 };
 
-</script>
+// 4. إعداد التتبع الخاص بالحسابات
+gtag('js', new Date());
+gtag('config', 'AW-10997329046');
+gtag('config', 'G-NR4CFG9TFJ', { 'anonymize_ip': true });
+
+// 5. تحميل سكريبت جوجل برمجياً لضمان أنه لن يشتغل إلا بعد ترتيب الأوامر السابقة بالكامل
+(function() {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=AW-10997329046";
+  document.head.appendChild(script);
+})();
